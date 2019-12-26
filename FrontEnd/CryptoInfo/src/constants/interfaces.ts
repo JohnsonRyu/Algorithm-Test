f10f57ed7903b397c86705b8d4cbc22a97e5b71a
JohnsonRyu <ryugihyeok@gmail.com>
2019-12-27 00:21:34 +0900

    ENH: marketInfo.ts 추가 및 interface 추가
    korbit api에서 MarketList를 가지고 올 수 있는 마땅한 방법이 없다. 모든 시장 현황 상제 정보 / 제약조건 을 통하여 symbol-market을 가지고 올 수 있지만, 해당 방법으로는 kor-name을 가지고 올수 없다. 그렇기 때문에 미리 정의 하고, api로 받아오는 정보와 병합 할 생각.

--- FrontEnd/CryptoInfo/src/constants/interfaces.ts
+++ FrontEnd/CryptoInfo/src/constants/interfaces.ts
@@ -2,3 +2,25 @@ export interface ITabType {
   menuName: string;
   render: () => JSX.Element;
 }
+
+export interface ICoinDetailed {
+  timestamp: number;
+  last: string;
+  open: string;
+  bid: string;
+  ask: string;
+  low: string;
+  high: string;
+  volume: string;
+  change: string;
+  changePercent: string;
+}
+
+export interface ICoinInfo {
+  currencyPair: string;
+  market: string;
+  symbol: string;
+  koreanName: string;
+}
+
+export interface IMarketItemInfo extends ICoinInfo, ICoinDetailed {}
---
+++ FrontEnd/CryptoInfo/src/constants/marketInfo.ts
@@ -0,0 +1,180 @@
+export const MARKETINFO = {
+  KRW: [
+    {
+      currencyPair: "bch_krw",
+      market: "KRW",
+      symbol: "BCH",
+      koreanName: "비트코인 캐시"
+    },
+    {
+      currencyPair: "fet_krw",
+      market: "KRW",
+      symbol: "FET",
+      koreanName: "페치"
+    },
+    {
+      currencyPair: "bnb_krw",
+      market: "KRW",
+      symbol: "BNB",
+      koreanName: "바이낸스 코인"
+    },
+    {
+      currencyPair: "loom_krw",
+      market: "KRW",
+      symbol: "LOOM",
+      koreanName: "룸네트워크"
+    },
+    {
+      currencyPair: "dai_krw",
+      market: "KRW",
+      symbol: "DAI",
+      koreanName: "다이"
+    },
+    {
+      currencyPair: "zil_krw",
+      market: "KRW",
+      symbol: "ZIL",
+      koreanName: "질리카"
+    },
+    {
+      currencyPair: "mkr_krw",
+      market: "KRW",
+      symbol: "MKR",
+      koreanName: "메이커"
+    },
+    {
+      currencyPair: "xlm_krw",
+      market: "KRW",
+      symbol: "XLM",
+      koreanName: "스텔라루멘"
+    },
+    {
+      currencyPair: "usdc_krw",
+      market: "KRW",
+      symbol: "USDC",
+      koreanName: "유에스디코인"
+    },
+    {
+      currencyPair: "med_krw",
+      market: "KRW",
+      symbol: "MED",
+      koreanName: "메디블록"
+    },
+    {
+      currencyPair: "btc_krw",
+      market: "KRW",
+      symbol: "BTC",
+      koreanName: "비트코인"
+    },
+    {
+      currencyPair: "bat_krw",
+      market: "KRW",
+      symbol: "베이직어텐션토큰",
+      koreanName: "BAT"
+    },
+    {
+      currencyPair: "gnt_krw",
+      market: "KRW",
+      symbol: "GNT",
+      koreanName: "골렘"
+    },
+    {
+      currencyPair: "poly_krw",
+      market: "KRW",
+      symbol: "POLY",
+      koreanName: "폴리매쓰"
+    },
+    {
+      currencyPair: "aergo_krw",
+      market: "KRW",
+      symbol: "AERGO",
+      koreanName: "아르고"
+    },
+    {
+      currencyPair: "bsv_krw",
+      market: "KRW",
+      symbol: "비트코인SV",
+      koreanName: "BSV"
+    },
+    {
+      currencyPair: "eth_krw",
+      market: "KRW",
+      symbol: "ETH",
+      koreanName: "이더리움"
+    },
+    {
+      currencyPair: "knc_krw",
+      market: "KRW",
+      symbol: "KNC",
+      koreanName: "카이버네트워크"
+    },
+    {
+      currencyPair: "snt_krw",
+      market: "KRW",
+      symbol: "SNT",
+      koreanName: "스테이터스"
+    },
+    {
+      currencyPair: "zrx_krw",
+      market: "KRW",
+      symbol: "ZRX",
+      koreanName: "제로엑스"
+    },
+    {
+      currencyPair: "eos_krw",
+      market: "KRW",
+      symbol: "EOS",
+      koreanName: "이오스"
+    },
+    {
+      currencyPair: "xrp_krw",
+      market: "KRW",
+      symbol: "XRP",
+      koreanName: "리플"
+    },
+    {
+      currencyPair: "link_krw",
+      market: "KRW",
+      symbol: "LINK",
+      koreanName: "체인링크"
+    },
+    {
+      currencyPair: "qtum_krw",
+      market: "KRW",
+      symbol: "QTUM",
+      koreanName: "퀀텀"
+    },
+    {
+      currencyPair: "etc_krw",
+      market: "KRW",
+      symbol: "ETC",
+      koreanName: "이더리움 클래식"
+    },
+    {
+      currencyPair: "trx_krw",
+      market: "KRW",
+      symbol: "TRX",
+      koreanName: "트론"
+    },
+    {
+      currencyPair: "omg_krw",
+      market: "KRW",
+      symbol: "OMG",
+      koreanName: "오미세고"
+    },
+    {
+      currencyPair: "ltc_krw",
+      market: "KRW",
+      symbol: "LTC",
+      koreanName: "라이트 코인"
+    },
+  ],
+  USDC: [
+    {
+      currencyPair: "btc_usdc",
+      market: "USDC",
+      symbol: "BTC",
+      koreanName: "비트코인"
+    }
+  ]
+};
