17d0c28dcd9dec6de1412b5b3a3cef151da9ff3b
JohnsonRyu <ryugihyeok@gmail.com>
2020-01-05 18:50:25 +0900

    ENH: SearchBar Input Atom 수정

--- FrontEnd/CryptoInfo/src/components/molecules/SearchBar.tsx
+++ FrontEnd/CryptoInfo/src/components/molecules/SearchBar.tsx
@@ -1,15 +1,17 @@
 import React from "react";
-
-import { Container } from "../atoms/Container";
 import { Icon } from "semantic-ui-react";
 
+import { Container } from "../atoms/Container";
+import { Input } from "../atoms/Input";
+import { MARKETLISTTEXT } from "../../constants/texts";
+
 export const SearchBar = () => (
-  <Container _height="60px" _content_align="center">
+  <Container _height="60px" _content_align="center" _borderBottom>
     <Container _width="60px" _content_align="center">
       <Icon name="search" size="large"/>
     </Container>
     <Container _width="255px" _content_align="center">
-      <input></input>
+      <Input _width="100%" placeholder={MARKETLISTTEXT.KOR.inputDefault}/>
     </Container>
     <Container _width="65px" _content_align="center">
       <Icon name="cancel" size="large"/>
